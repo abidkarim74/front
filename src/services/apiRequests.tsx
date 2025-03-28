726bb61f58d9d2b937aecff4b5122623212f0321
abidkarim <abidkarim737@gmail.com>
2025-03-28 18:47:55 +0500

    Initial commit for React app

---
+++ src/services/apiRequests.tsx
@@ -0,0 +1,56 @@
+import axiosJWT from "./axiosInstance";
+
+export const postRequest = async (
+  data: any,
+  url: string,
+  accessToken:string | null,
+  setLoading:React.Dispatch<React.SetStateAction<boolean>>,
+  setError: React.Dispatch<React.SetStateAction<null | string>>
+) => {
+  try {
+    setError(null);
+    setLoading(true);
+    
+    const res = await axiosJWT.post(url, data);
+    console.log("Response: ",res);
+
+    return res.data;
+
+  } catch (err: any) {
+    setError(err.message);
+    
+  } finally {
+    setLoading(false);
+
+  }
+}
+
+
+export const getRequest = async (
+  url: string,
+  accessToken:string | null,
+  setLoading:React.Dispatch<React.SetStateAction<boolean>>,
+  setError: React.Dispatch<React.SetStateAction<null | string>>
+) => {
+  try {
+    setError(null);
+    setLoading(true);
+    const res = await axiosJWT.get(url,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+        withCredentials: true,
+      });
+
+    return res.data;
+
+  } catch (err: any) {
+    setError(err.message);
+    
+  } finally {
+    setLoading(false);
+
+  }
+}
+
